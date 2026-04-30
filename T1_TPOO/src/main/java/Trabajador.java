733510af7890bc1745605b64f2f1163d1cfe48ae
UCC20305 <UCC20305@UCC20305>
2026-04-29 20:25:26 -0500

    Creamos la clase

---
+++ T1_TPOO/src/main/java/Trabajador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author UCC20305
+ */
+public class Trabajador {
+    
+    private String nombre;
+    private String tipoDoc;
+    private String numDoc;
+    private String regimen;
+    private String pension;
+    private double sueldoBase;
+    private boolean tieneHijos;
+    private boolean esNocturno;
+}
